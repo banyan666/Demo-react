b763d5197456b827a6233460a1e4a33957a5df7c
ban_yan <15029296293@163.com>
2024-10-21 14:19:11 +0800

    新增炫酷border

--- src/pages/Page1/index.tsx
+++ src/pages/Page1/index.tsx
@@ -1,13 +1,17 @@
 import {Button} from 'antd'
 import {useModel} from "umi";
+import './index.css'
 
 
 export default function HomePage() {
     const {title, setTitle} = useModel('home')
     return (
-        <div>
+        <div style={{width:'100%'}}>
             <Button type="primary" onClick={() => setTitle('修改的title')}>我是antd的按钮</Button>
             <div>{title}</div>
+            <div style={{width:'100%',height:300}}>
+                <div className={'card'}>炫酷的box</div>
+            </div>
         </div>
     );
 }
